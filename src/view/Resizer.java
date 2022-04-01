abef7ff6db2671d035bb99d7dd1fe8740e881756
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2022-04-01 23:49:19 +0200

    Added vertical screen support

--- src/view/Resizer.java
+++ src/view/Resizer.java
@@ -14,13 +14,17 @@ public class Resizer {
 	    int dimY=0;
 		
 		if(checkVertical()) {
-			System.out.println("verticale");
+			System.out.println("verticale");		
+			dimX=(int) (screenSize.getWidth()/factor);
+			dimY=(int) ((screenSize.getWidth()*16/9)/factor);	
 		}else {		
 			System.out.println("orizzontale");    	
 			if(checkRatio()) {
+				System.out.println("RatioCorretto 16/9");    	
 				dimX=(int) (screenSize.getWidth()/factor);
 				dimY=(int) (screenSize.getHeight()/factor);
 			} else {
+				System.out.println("RatioSbagliato != 16/9");  
 				dimX=(int) ((screenSize.getHeight()*16/9)/factor);
 				dimY=(int) (screenSize.getHeight()/factor);
 			}	
@@ -34,6 +38,5 @@ public class Resizer {
 
 	private boolean checkVertical() {
 		return(screenSize.getWidth()<screenSize.getHeight());
-	}
-		
+	}	
 }
