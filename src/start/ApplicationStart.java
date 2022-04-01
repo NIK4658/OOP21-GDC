b631bb67bdd8dbf6f784e0a641846c3b9741b070
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-04-01 19:47:18 +0200

    Changed ApplicationStart for run Roulette

--- src/start/ApplicationStart.java
+++ src/start/ApplicationStart.java
@@ -1,12 +1,22 @@
 package start;
 
-import view.access.AccessMenu;
+import roulette.gui.Roulette;
 
 /**
  * Starts the application.
  */
 public final class ApplicationStart {
-    public static void main(String[] args) {
-        new AccessMenu();
+    
+    public static void main(final String[] args) {
+        new Roulette();
+//        //
+//        final int numberPlayed = roulette.numberPlayed();
+//        final int numberCameOut = roulette.numberCameOut();
+//        //numeroVincente
+        //winning number
+//        System.out.println("Numero giocato " + numberPlayed);
+//        System.out.println("Numero uscito " + numberCameOut);
+//        System.out.println("Numero preso: " + (numberPlayed == numberCameOut));
     }
+    
 }
