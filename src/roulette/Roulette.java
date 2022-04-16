373338e1f50118b93c7ff8f9a7d935a94295ccfd
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-04-16 10:55:36 +0200

    Create DisplayWinningNumbers.java

--- src/roulette/Roulette.java
+++ src/roulette/Roulette.java
@@ -4,13 +4,19 @@ import java.util.Random;
 
 import javax.swing.Timer;
 
+import view.roulette.DisplayWinningNumbers;
+
+//da eliminare winningNumbers e metodo spin deve tornare un RouletteNumber
 public class Roulette {
     private static final int DELAY = 3000;
     private final Random random;
     private final WinningNumbers winningNumbers;
-    public Roulette() {
-        random = new Random();
-        winningNumbers = new WinningNumbers();
+    private final DisplayWinningNumbers l;
+    
+    public Roulette(final DisplayWinningNumbers l) {
+        this.random = new Random();
+        this.winningNumbers = new WinningNumbers();
+        this.l = l;
     }
     
     public void start() {
@@ -23,5 +29,6 @@ public class Roulette {
         System.out.println(a);
         this.winningNumbers.add(a);
         System.out.println(winningNumbers);
+        l.display(this.winningNumbers);
     }
 }
