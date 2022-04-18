eba6c27b8da9bb7e5310200aacf4e64b5ddd89b3
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2022-04-18 12:24:50 +0200

    Updated test file for account management

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -1,5 +1,34 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+
+import account.AccountManager;
+import account.AccountManagerImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class AccountTest {
 
+    private AccountManager account;
+    
+    @Before
+    public void initAccount() {
+        this.account = new AccountManagerImpl();
+    }
+    
+    
+    //test deposito
+    @Test
+    public void testDeposit() {
+        assertTrue(false);
+    }
+
+    
+    //test ritiro
+    @Test
+    public void testWithdraw() {
+        assertTrue(true);
+    }
+    
 }
