4757460e2f7f54fc3d7a2f54686c688a895d0e23
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-04-19 21:16:53 +0200

    Add labelAlert and check for max/min import and other

--- src/view/menu/account/BalancePanel.java
+++ src/view/menu/account/BalancePanel.java
@@ -17,9 +17,11 @@ import javax.swing.JPanel;
 import account.AccountManager;
 import view.GridBagConstraintsConstructor;
 
-//pannello GESTIONE SALDO, sistemare ripetizioni
+//pannello GESTIONE SALDO, sistemare ripetizioni e creare funzioni per check amount
 public class BalancePanel extends JPanel {
     
+    private static final double MAX_IMPORT = 10000;
+    private static final double MIN_IMPORT = 15;
     private final AccountManager account;
     
     public BalancePanel(final AccountManager account) {
@@ -31,37 +33,60 @@ public class BalancePanel extends JPanel {
         //aggiungere "titolo" BALANCE
         final JLabel labelDeposit = new JLabel(currencySymbol);
         final JLabel labelWithdraw = new JLabel(currencySymbol);
-        final JLabel labelAmount = new JLabel("Amount: ");
+        final JLabel labelBalance = new JLabel("Balance: ");
+        final JLabel labelAlert = new JLabel();
     
         final NumberFormat format = DecimalFormat.getInstance();
-        final NumberFormat formatAmount = DecimalFormat.getCurrencyInstance();
-        format.setMaximumIntegerDigits(4);
+        final NumberFormat formatBalance = DecimalFormat.getCurrencyInstance();
+//        format.setMaximumIntegerDigits(4);
         format.setMinimumFractionDigits(2);
         format.setMaximumFractionDigits(2);
         format.setRoundingMode(RoundingMode.HALF_UP);
         final JFormattedTextField fieldDeposit = new JFormattedTextField(format);
         final JFormattedTextField fieldWithdraw = new JFormattedTextField(format);
-        final JFormattedTextField fieldAmount = new JFormattedTextField(formatAmount);
+        final JFormattedTextField fieldBalance = new JFormattedTextField(formatBalance);
         fieldDeposit.setColumns(10);
         fieldDeposit.setValue(0);
         fieldWithdraw.setColumns(10);
         fieldWithdraw.setValue(0);
-        fieldAmount.setColumns(10);
-        fieldAmount.setEditable(false);
-        fieldAmount.setValue(this.getAmount());
+        fieldBalance.setColumns(10);
+        fieldBalance.setEditable(false);
+        fieldBalance.setValue(this.getBalance());
         
         //Eliminare rep con altro bottone(es. crea funzione)
         final JButton buttonDeposit = new JButton("Deposit");
         buttonDeposit.addActionListener(e -> {
             final String deposit = fieldDeposit.getText().replace(".", "").replace(",", ".");
-            this.setDeposit(Double.parseDouble(deposit));
-            fieldAmount.setValue(this.getAmount());
+            final double amount = Double.parseDouble(deposit);
+            if (amount < MIN_IMPORT) {
+                labelAlert.setText("The minimum amount must be at least " + formatBalance.format(MIN_IMPORT));
+            } else if (amount > MAX_IMPORT) {
+                labelAlert.setText("The maximum amount must be amaximum " + formatBalance.format(MAX_IMPORT));
+            } else {
+                if (this.setDeposit(amount)) {
+                    fieldBalance.setValue(this.getBalance());
+                    labelAlert.setText("Successful deposit of " + formatBalance.format(amount));
+                } else {
+                    labelAlert.setText("Unsuccessful deposit");
+                }
+            }
         });
         final JButton buttonWithdraw = new JButton("Withdraw");
         buttonWithdraw.addActionListener(e -> {
             final String withdraw = fieldWithdraw.getText().replace(".", "").replace(",", ".");
-            this.setWithdraw(Double.parseDouble(withdraw));
-            fieldAmount.setValue(this.getAmount());
+            final double amount = Double.parseDouble(withdraw);
+            if (amount < MIN_IMPORT) {
+                labelAlert.setText("The minimum amount must be at least " + formatBalance.format(MIN_IMPORT));
+            } else if (amount > MAX_IMPORT) {
+                labelAlert.setText("The maximum amount must be a maximum " + formatBalance.format(MAX_IMPORT));
+            } else {
+                if (this.setWithdraw(amount)) {
+                    fieldBalance.setValue(this.getBalance());
+                    labelAlert.setText("Successful withdraw of " + formatBalance.format(amount));
+                } else {
+                    labelAlert.setText("Unsuccessful withdraw");
+                }
+            }
         });
     
         this.add(labelDeposit);
@@ -70,23 +95,22 @@ public class BalancePanel extends JPanel {
         this.add(labelWithdraw, GridBagConstraintsConstructor.get(0, 1, 0));
         this.add(fieldWithdraw, GridBagConstraintsConstructor.get(1, 1, 0));
         this.add(buttonWithdraw, GridBagConstraintsConstructor.get(2, 1, 0));
-        this.add(labelAmount, GridBagConstraintsConstructor.get(0, 2, 0));
-        this.add(fieldAmount, GridBagConstraintsConstructor.get(1, 2, 0));
+        this.add(labelBalance, GridBagConstraintsConstructor.get(0, 2, 0));
+        this.add(fieldBalance, GridBagConstraintsConstructor.get(1, 2, 0));
+        this.add(labelAlert, GridBagConstraintsConstructor.get(1, 3, 0));
     }
 
-    private double getAmount() {
+    private double getBalance() {
         return this.account.getBalance();
     }
     
-    private void setDeposit(final double deposit) {
-        //manca il check per numeri negativi
-        this.account.deposit(deposit);
+    private boolean setDeposit(final double deposit) {
+        return this.account.deposit(deposit);
     }
     
     
-    private void setWithdraw(final double withdraw) {
-        //manca il check per numeri negativi
-        this.account.withdraw(withdraw);
+    private boolean setWithdraw(final double withdraw) {
+        return this.account.withdraw(withdraw);
     }
 
 }
