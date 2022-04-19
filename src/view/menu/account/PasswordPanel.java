e71cd46ea560dc13ea4aac14abe8c785f3a60af1
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-04-19 21:19:46 +0200

    Add labelAlert for password changed successfully or unsuccessfully

--- src/view/menu/account/PasswordPanel.java
+++ src/view/menu/account/PasswordPanel.java
@@ -24,26 +24,38 @@ public class PasswordPanel extends JPanel {
         this.setLayout(new GridBagLayout());
         this.setBackground(new Color(68, 87, 96));     
         
-        final JLabel labelPassword = new JLabel("New Password: ");
-        final JLabel labelNewPassword = new JLabel("Confirm Password: ");
+        final JLabel labelPassword = new JLabel("Insert new Password: ");
+        final JLabel labelNewPassword = new JLabel("Confirm new Password: ");
         final JTextField fieldPassword = new JTextField(10);
-        fieldPassword.setEditable(false);//momentaneamente disabilitato, bisogna implementare una JDialog 
+//        fieldPassword.setEditable(false);//momentaneamente disabilitato, bisogna implementare una JDialog 
         final JTextField fieldNewPassword = new JTextField(10);
         final JButton buttonPassword = new JButton("Change");
+        final JLabel labelAlert = new JLabel();
+        
         buttonPassword.addActionListener(e -> {//aggiungere password non valida
-            if (new ConfirmPassword(frame, account, " to change Password").isPasswordConfirmed()) {
-                this.setPassword(fieldNewPassword.getText());
+            if (fieldPassword.getText().equals(fieldNewPassword.getText())) {
+                if (new ConfirmPassword(frame, account, " to change Password").isPasswordConfirmed()) {
+                    if (this.setPassword(fieldNewPassword.getText())) {
+                        labelAlert.setText("Password changed");
+                        fieldPassword.setText("");
+                        fieldNewPassword.setText("");
+                    }
+                }
+            } else {
+                labelAlert.setText("Different passwords entered in the fields");
             }
+            
         });
         this.add(labelPassword, GridBagConstraintsConstructor.get(0, 0, 0));
         this.add(fieldPassword, GridBagConstraintsConstructor.get(1, 0, 0));
         this.add(labelNewPassword, GridBagConstraintsConstructor.get(0, 1, 0));
         this.add(fieldNewPassword, GridBagConstraintsConstructor.get(1, 1, 0));
         this.add(buttonPassword, GridBagConstraintsConstructor.get(2, 2, 0));
+        this.add(labelAlert, GridBagConstraintsConstructor.get(1, 3, 0));
     }
 
-    private void setPassword(final String password) {
-        this.account.changePass(password);
+    private boolean setPassword(final String password) {
+        return this.account.changePass(password);
     }
     
 }
