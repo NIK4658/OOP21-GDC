e3c3705c6584b569ff29ef9e7dc54ee4a1fa2134
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2022-04-20 22:31:45 +0200

    Added buttons to select the chips, the reset button now works, inserted the image of a chip based on the bet, the game now modifies the user's balance and the refactoring has been performed

--- src/blackjack/Gui.java
+++ src/blackjack/Gui.java
@@ -18,20 +18,23 @@ import java.awt.Image;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import ex.ExImageModifier;
+import view.gui.MainGui;
 
 
 public class Gui extends JFrame{
 
     private static final long serialVersionUID = 1L;
     
-    private int playerValue;
-    private int dealerValue;
-    private int saldo = 10;
-    private int puntata = 0;
-    private boolean rotatefirstcard = false;
-    private final Game g = new GameImpl();
+    private int puntata;
+    private int chipvalue = 1;
+    
+    List<JLabel> dCards;
+    List<JLabel> pCards;
+    private final Game g;
     
     public Gui(final Dimension dim, AccountManager account) {
+        
+        g = new GameImpl(account);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         //JPanel con immagine di sfondo
@@ -47,11 +50,27 @@ public class Gui extends JFrame{
         final JButton stay = new JButton("Stand");
         final JButton Double = new JButton("Double");
         final JButton reset = new JButton("Reset");      
+        
+        final JButton chip1 = new JButton("1"); 
+        final JButton chip2 = new JButton("5");
+        final JButton chip3 = new JButton("25");
+        final JButton chip4 = new JButton("100");    
+        final JButton chip5 = new JButton("500");
+        
+        
         final List<JButton> l = new ArrayList<>();
         l.add(draw);
         l.add(stay);
         l.add(Double);
-        l.add(reset);       
+        l.add(reset);   
+        
+        l.add(chip1);
+        l.add(chip2);
+        l.add(chip3);
+        l.add(chip4);
+        l.add(chip5);
+        
+        
         int i = 0;
         for (final JButton jb : l) { 
             jb.setPreferredSize(new Dimension(130, 100));
@@ -70,7 +89,7 @@ public class Gui extends JFrame{
         //JPanel a layer che mostra le carte del giocatore CENTER
         final JLayeredPane playerCardsPanel = new JLayeredPane();
         //playerCardsPanel.setLayout(null); //da rivedere
-        final List<JLabel> pCards = new LinkedList<>(); //lista di JLabel, ciascuna sarà una carta del giocatore
+        pCards = new LinkedList<>(); //lista di JLabel, ciascuna sarà una carta del giocatore
         
         //Punteggio player
         final JLabel playerpoints = new JLabel();
@@ -79,16 +98,16 @@ public class Gui extends JFrame{
         playerpoints.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 20));
         playerCardsPanel.add(playerpoints, 0);
         
-        final JLabel puntata2 = new JLabel("0 €");
-        puntata2.setForeground(Color.WHITE);
-        puntata2.setBounds(350, 20, 150, 150);
-        puntata2.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 30));
-        playerCardsPanel.add(puntata2, 0);
+        final JLabel amountbet = new JLabel("0 €");
+        amountbet.setForeground(Color.WHITE);
+        amountbet.setBounds(350, 20, 150, 150);
+        amountbet.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 30));
+        playerCardsPanel.add(amountbet, 0);
         
         
-        final JButton soldi = new JButton("Punta 1€");
-        soldi.setBounds(350, 120, 90, 30);
-        playerCardsPanel.add(soldi, 0);
+        final JButton bet = new JButton("Punta");
+        bet.setBounds(350, 120, 90, 30);
+        playerCardsPanel.add(bet, 0);
         
         final JButton resetta = new JButton("Resetta");
         resetta.setBounds(350, 155, 90, 30);
@@ -112,7 +131,7 @@ public class Gui extends JFrame{
         //dealerCardsPanel.setLayout(null); //da rivedere
         dealerCardsPanel.setPreferredSize(new Dimension(300, 300));
         //Lista delle carte del dealer
-        final List<JLabel> dCards = new LinkedList<>();
+        dCards = new LinkedList<>();
         final JLabel dpoints = new JLabel();
         dpoints.setForeground(Color.WHITE);
         dpoints.setBounds(530, 20, 150, 150);
@@ -121,7 +140,7 @@ public class Gui extends JFrame{
         dealerCardsPanel.add(dpoints, 0);
         
         
-        final JLabel saldo = new JLabel("Saldo: " + this.saldo);
+        final JLabel saldo = new JLabel("Saldo: " + account.getBalance());
         final JLabel puntata = new JLabel("Puntata: " + this.puntata);
         saldo.setForeground(Color.WHITE);
         saldo.setBounds(100, 80, 150, 150);
@@ -156,13 +175,13 @@ public class Gui extends JFrame{
 
         //da fare refactoring
         draw.addActionListener(e -> {   
-            if (this.playerValue < 21) {
+            if (g.getPlayerPoints() < 21) {
                 g.askCard();
                 addCard(pCards, g.getPlayerHand().get(g.getPlayerHand().size() - 1), playerCardsPanel);
                 playerpoints.setText(String.valueOf(g.calculatePoints(g.getPlayerHand())));
             } 
       
-            if (this.playerValue >= 21) {
+            if (g.getPlayerPoints() >= 21) {
                 stay.doClick();
             }
         });
@@ -170,36 +189,28 @@ public class Gui extends JFrame{
         //codice ripetuto
         stay.addActionListener(e -> {
             //g.nextDealerMove();
-//      ~FEDE
-//            dCards.get(1).setIcon(new ImageIcon(new ImageModifier()
-//                    .scale(new CardImpl(g.getDealerHand().get(1).getSuit(), 
-//                            g.getDealerHand().get(1).getValue()).getImg(),
-//                            new Dimension(150, 150))));
             dCards.get(1).setIcon(new ImageIcon(new CardImpl(g.getDealerHand().get(1).getSuit(), 
-                            g.getDealerHand().get(1).getValue()).getImg().getScaledInstance(103,150,Image.SCALE_SMOOTH)));
-            
+                    g.getDealerHand().get(1).getValue()).getImg().getScaledInstance(100, 150, Image.SCALE_SMOOTH))); 
             dpoints.setText(String.valueOf(g.calculatePoints(g.getDealerHand())));
             
-            this.dealerValue = g.calculatePoints(g.getDealerHand());
             System.out.println("ciao");
-            while (this.dealerValue < 17) {
+            while (g.getDealerPoints() < 17) {
                 //aggiunge altre carte nel caso non basti la prima
                 g.nextDealerMove();
-                System.out.println(g.getDealerHand() + "ciao");
                 addCardDealer(dCards, g.getDealerHand().get(g.getDealerHand().size() - 1), dealerCardsPanel);
-                this.dealerValue = g.calculatePoints(g.getDealerHand());
                 dpoints.setText(String.valueOf(g.calculatePoints(g.getDealerHand())));
             }
             
             //dice chi ha vinto e i punteggi
             //System.out.println(checkWin(this.playerValue, this.dealerValue));
+            
             if (g.checkWin()) {
-                this.saldo += this.puntata;
+                account.deposit(this.puntata); 
             } else {
-                this.saldo -= this.puntata;
+                account.withdraw(this.puntata); 
             }
             
-            saldo.setText("Saldo: " + this.saldo);
+            saldo.setText("Saldo: " + account.getBalance());
             
             //disattiva i pulsanti
             draw.setEnabled(false);
@@ -212,19 +223,98 @@ public class Gui extends JFrame{
         
         
         
-        soldi.addActionListener(e -> {  
-            this.puntata++;
-            puntata2.setText(this.puntata + " €");
+        chip1.addActionListener(e -> {  
+            this.chipvalue = 1;
+        });
+        
+        chip2.addActionListener(e -> {  
+            this.chipvalue = 5;
+        });
+        
+        chip3.addActionListener(e -> {  
+            this.chipvalue = 25;
+        });
+        
+        chip4.addActionListener(e -> {  
+            this.chipvalue = 100;
+        });
+        
+        chip5.addActionListener(e -> {  
+            this.chipvalue = 500;
+        });
+        
+        bet.addActionListener(e -> {  
+            this.puntata += this.chipvalue;
+            bet.setText("");
+            
+            bet.removeAll();
+            
+            final JPanel jp = new JPanel(new BorderLayout());
+            
+            //jp.setPreferredSize(new Dimension(100, 100));
+            
+            
+            
+            final JLabel punt = new JLabel(String.valueOf(this.puntata), SwingConstants.CENTER);
+            punt.setForeground(Color.WHITE);
+            
+            jp.setOpaque(false);
+            
+            //jp.setBackground(Color.BLACK);
+            
+            jp.add(punt, BorderLayout.CENTER);
+            
+            
+            bet.add(jp, BorderLayout.CENTER);
+            bet.setIcon(chooseChip(this.puntata));
+            
+            
+            
+            amountbet.setText(this.puntata + " €");
         });
         
         resetta.addActionListener(e -> {  
             this.puntata = 0;
-            puntata2.setText(this.puntata + " €");
+            amountbet.setText(this.puntata + " €");
+        });
+       
+        reset.addActionListener(e -> {  
+            this.puntata = 0;
+            amountbet.setText(this.puntata + " €");
+            puntata.setText("Puntata: " + this.puntata);
+            resetta.setEnabled(true);
+            bet.setEnabled(true);
+            conferma.setEnabled(true);
+            
+            draw.setEnabled(false);
+            stay.setEnabled(false);
+            reset.setEnabled(false);
+            Double.setEnabled(false);
+            
+            dpoints.setText("");
+            playerpoints.setText("");
+            
+            
+            for(final JLabel j : pCards) {
+                playerCardsPanel.remove(j);
+            }
+            
+            for(final JLabel j : dCards) {
+                dealerCardsPanel.remove(j);
+            }
+
+            dCards = new LinkedList<>();
+            pCards = new LinkedList<>();     
+            playerCardsPanel.revalidate();
+            playerCardsPanel.repaint();
+            dealerCardsPanel.revalidate();
+            dealerCardsPanel.repaint();
+            g.newTurn();
         });
         
         conferma.addActionListener(e -> {  
             resetta.setEnabled(false);
-            soldi.setEnabled(false);
+            bet.setEnabled(false);
             conferma.setEnabled(false);
             
             draw.setEnabled(true);
@@ -252,21 +342,43 @@ public class Gui extends JFrame{
     }
     
     
+    private ImageIcon chooseChip(final int puntata) {
+        
+        if (puntata <  5) {
+            final Image img = new ImageIcon("res/img/fiches/empty/1.png").getImage();
+            return new ImageIcon(img.getScaledInstance(40, 40, Image.SCALE_SMOOTH));
+        }
+        
+        if (puntata < 25) {
+            final Image img = new ImageIcon("res/img/fiches/empty/5.png").getImage();
+            return new ImageIcon(img.getScaledInstance(40, 40, Image.SCALE_SMOOTH));
+        }
+        
+        if (puntata < 100) {
+            final Image img = new ImageIcon("res/img/fiches/empty/25.png").getImage();
+            return new ImageIcon(img.getScaledInstance(40, 40, Image.SCALE_SMOOTH));
+        }
+        
+        if (puntata < 500) {
+            final Image img = new ImageIcon("res/img/fiches/empty/100.png").getImage();
+            return new ImageIcon(img.getScaledInstance(40, 40, Image.SCALE_SMOOTH));
+        }
+        
+        final Image img = new ImageIcon("res/img/fiches/empty/500.png").getImage();
+        return new ImageIcon(img.getScaledInstance(40, 40, Image.SCALE_SMOOTH)); 
+    }
+
+
     private void addCard(final List<JLabel> cards, final Card c, final JLayeredPane p) {
         cards.add(new JLabel());
         cards.get((cards.size() - 1)).setBounds(575 + ((cards.size() - 1) * 20),
                 80 - ((cards.size() - 1) * 15), 150, 150);  //setbounds temporaneo?
         //setta al JLabel di index i l'immagine della nuova carta.
-//~FEDE
-//        cards.get((cards.size() - 1)).setIcon(new ImageIcon(new ImageModifier().scale(c.getImg(),
-//                new Dimension(150, 150))));
-        cards.get((cards.size() - 1)).setIcon(new ImageIcon(c.getImg().getScaledInstance(103, 150, Image.SCALE_SMOOTH)));
-        
-        
+        cards.get((cards.size() - 1)).setIcon(new ImageIcon(c.getImg()
+                .getScaledInstance(103, 150, Image.SCALE_SMOOTH)));
         //aggiunge la carta al pannello al layer 1
         p.add(cards.get((cards.size() - 1)), 0);
-        //punteggio temporaneo giocatore
-        this.playerValue = g.calculatePoints(g.getPlayerHand());   
+
     }
     
     //da eliminare
@@ -275,14 +387,10 @@ public class Gui extends JFrame{
         cards.get((cards.size() - 1)).setBounds(575 + ((cards.size() - 1) * 35),
                 80 + ((cards.size() - 1) * 10), 150, 150);  //setbounds temporaneo?
         //setta al JLabel di index i l'immagine della nuova carta.
-//~FEDE
-//        cards.get((cards.size() - 1)).setIcon(new ImageIcon(new ImageModifier().scale(c.getImg(),
-//                new Dimension(150, 150))));
-        cards.get((cards.size() - 1)).setIcon(new ImageIcon(c.getImg().getScaledInstance(103, 150, Image.SCALE_SMOOTH)));
+        cards.get((cards.size() - 1)).setIcon(new ImageIcon(c.getImg()
+                .getScaledInstance(103, 150, Image.SCALE_SMOOTH)));
         //aggiunge la carta al pannello al layer 1
         p.add(cards.get((cards.size() - 1)), 0);
-        //punteggio temporaneo giocatore 
-        this.dealerValue = g.calculatePoints(g.getDealerHand());   
     }
     
     
@@ -327,11 +435,8 @@ public class Gui extends JFrame{
      * Testing. Da eliminare in seguito.
      */
     public static void main(final String[] args) {      
-        //new DeckImpl().showPreciseValue(5); 
-        //new DeckImpl().showPreciseSuit(Suits.DIAMONDS);
-        //new DeckImpl().showPreciseCard(new CardImpl());
-        //new DeckImpl().showAllCards();
         //new Gui(new Dimension(1280, 720));
+        new MainGui();
     }
     
 }
