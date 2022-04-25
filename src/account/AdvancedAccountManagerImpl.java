36fd0f4a9a448735f49660b95a8632325a4b7ba3
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2022-04-25 19:23:38 +0200

    Added Advanced versions of AccountManager and BalanceManager

---
+++ src/account/AdvancedAccountManagerImpl.java
@@ -0,0 +1,5 @@
+package account;
+
+public class AdvancedAccountManagerImpl extends SimpleAccountManagerImpl {
+
+}
---
+++ src/account/AdvancedBalanceManagerImpl.java
@@ -0,0 +1,10 @@
+package account;
+
+public class AdvancedBalanceManagerImpl extends SimpleBalanceManagerImpl {
+
+    AdvancedBalanceManagerImpl(String username) {
+        super(username);
+        // TODO Auto-generated constructor stub
+    }
+
+}
