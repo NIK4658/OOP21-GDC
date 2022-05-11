8940a676e23621c757a1a7c7a37270d72b1da201
Vale1706 <65721801+Vale1706@users.noreply.github.com>
2022-05-11 12:19:26 +0200

    Warning code refactoring

--- src/start/Warning.java
+++ src/start/Warning.java
@@ -25,41 +25,16 @@ public class Warning extends JDialog {
         
         //Definition of center and south JPanel
     	
-    	JPanel center = new JPanel ();
-    	JPanel south = new JPanel ();
-        center.setBorder ( new TitledBorder ( new EtchedBorder (), "WARNING!!" ) );
+        JPanel center = new JPanel();
+    	JPanel south = new JPanel();
+        center.setBorder(new TitledBorder(new EtchedBorder(), "WARNING!!"));
 
         // Definition of a Text area with terms and condiction
 
-        JTextArea textArea = new JTextArea ( 15,30 );
+        JTextArea textArea = new JTextArea(15, 30);
         textArea.setBackground(Color.RED);
-        /*textArea.setText("1.1 William Hill RacingTV is only available to William Hill account holders, "
-        		+ "subject to the provisions of the remaining terms and conditions below.\n"
-        		+ "1.2 Race viewing is available to UK and Ireland residents only and may not \n"
-        		+ "be available to customers using foreign configured IP addresses. Please contact "
-        		+ "your Internet Service Provider for more information.\n"
-        		+ "1.3 William Hill RacingTV is available on a race by race basis based  \n"
-        		+ "on you placing at least a £1.00 single bet or a £0.50 each-way single "
-        		+ "bet (total stake £1.00), or at least a £2.00 double bet or a £1.00 each-way "
-        		+ "double bet \n"
-        		+ " (total stake £2.00), or at least a £3.00 treble bet or a £1.50 each-way \n"
-        		+ "treble bet (total stake £3.00) \n"
-        		+ "on the race(s) you wish to view. For multiple bet types the equivalent\n"
-        		+ " stake of £1.00 per event \n"
-        		+ "must be spent to view each additional race.\n"
-        		+ "1.4 Antepost, totePool and special bets do not apply.\n"
-        		+ "1.5 However, William Hill reserve the right to alter these conditions \n"
-        		+ "for access or introduce a fee, subscription or other charge for access \n"
-        		+ "to William Hill RacingTV in \n"
-        		+ "the future. You will be made aware of the introduction of any charge or \n"
-        		+ "condition before it is introduced.\n"
-        		+ "1.6 William Hill may refuse access to William Hill RacingTV to any \n "
-        		+ "customer \n"
-        		+ "at its absolute discretion.\n"
-        		+ "1.7 By accessing William Hill RacingTV you accept these terms and conditions.\n"
-        		+ "");*/
         
-        
+        //lettura da file
 
         BufferedReader in = null;
         try {
@@ -70,19 +45,21 @@ public class Warning extends JDialog {
             }
         } catch (IOException e) {
         } finally {
-            try { in.close(); } catch (Exception ex) { }
+            try { 
+                in.close(); 
+            } catch (Exception ex) { }
         }
         
         // set textArea non-editable
-        textArea.setEditable ( false ); 
+        textArea.setEditable(false); 
         
         //Putting the Text area on a scroll panel
         
-        JScrollPane scrollText = new JScrollPane ( textArea );
+        JScrollPane scrollText = new JScrollPane(textArea);
         
         //Setting scroll option only vertical
         
-        scrollText.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+        scrollText.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scrollText.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         
         //Definition of a ceckbox and button for confirm terms and conditions
@@ -96,21 +73,17 @@ public class Warning extends JDialog {
 
         ActionListener actionListener = new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
-              AbstractButton abstractButton = (AbstractButton) actionEvent.getSource();
-              boolean selected = abstractButton.getModel().isSelected();
+                AbstractButton abstractButton = (AbstractButton) actionEvent.getSource();
+                boolean selected = abstractButton.getModel().isSelected();
               
-              if(selected==true) 
-              {
-            	  accept.setEnabled(true);
-            	  
-              }
+                if (selected == true) {
+                    accept.setEnabled(true);
+                }
               
-              else 
-              {
+                else {
+                    accept.setEnabled(false);
                   
-                  accept.setEnabled(false);
-                  
-              }
+                }
             
             }
             
@@ -118,18 +91,18 @@ public class Warning extends JDialog {
         
         //Adding the components to the panels
         
-        center.add ( scrollText );
-        south.add ( ceckbox );
-        south.add ( accept);
+        center.add(scrollText);
+        south.add(ceckbox);
+        south.add(accept);
         
         // Setting Frame
         ceckbox.addActionListener(actionListener);
-        JFrame frame = new JFrame ("WARNING");
-        frame.add ( center ,BorderLayout.CENTER);
+        JFrame frame = new JFrame("WARNING");
+        frame.add(center, BorderLayout.CENTER);
         frame.add(south, BorderLayout.SOUTH);
-        frame.pack ();
-        frame.setLocationRelativeTo ( null );
-        frame.setVisible ( true );
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
     	
