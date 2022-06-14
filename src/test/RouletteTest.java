7fdcd52347935bf3c90294ee63850e8a9becbe6a
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-06-14 09:58:33 +0200

    Create test junit for validWinningNumber and validIntervalWinningNumber

--- src/test/RouletteTest.java
+++ src/test/RouletteTest.java
@@ -1,31 +1,50 @@
 package test;
 
 
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import view.menu.games.roulette.Table;
+import roulette.Roulette;
+import roulette.RouletteNumber;
 
 
 public class RouletteTest {
     
-    private Table roulette;
+    private Roulette roulette;
+    private int testcases;
     
     @Before
     public void initRoulette() {
-        roulette = new Table();
+        roulette = new Roulette();
+        testcases = 1000;
     }
     
-    //testa se giocato numero compreso tra 0 e 36
+    //testa se la roulette ritorna un numero compreso tra 0 e 36 in 1000 casi
     @Test
-    public void testValidNumberPlayed () {
-//        int numberPlayed;
-//        for (int i=0; i<100; i++) {
-//            numberPlayed = roulette.numberPlayed();
-//            assertTrue(numberPlayed >= 0 && numberPlayed <= 36);
-//        }
+    public void testValidWinningNumber() {
+        for (int i = 0; i < testcases; i++) {
+            final int n = roulette.spin().getValue();
+            assertTrue(n >= 0 && n <= 36);
+        }
+    }
+
+    //testa se la roulette ritorna tutti e 37 i numeri in 1000 casi
+    @Test
+    public void testValidIntervalWinningNumber() {
+        final List<Integer> valuesWinningNumbers = new LinkedList<>();
+        for (int i = 0; i < testcases; i++) {
+            final int n = roulette.spin().getValue();
+            if (!valuesWinningNumbers.contains(n)) {
+                valuesWinningNumbers.add(n);
+            }   
+        }
+        assertSame(valuesWinningNumbers.size(), 37);
     }
     
     //testa se uscito numero compreso tra 0 e 36
