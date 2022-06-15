f23aa45589d6147fc375ab6abd4351bf9f2398e8
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-06-15 20:39:47 +0200

    changed implementation of RouletteManager
    Changed type static fields from int to double. Changed the verification of the bet ownership.

--- src/roulette/manageRoulette/ManageRoulette.java
+++ src/roulette/manageRoulette/ManageRoulette.java
@@ -6,65 +6,72 @@ import roulette.RouletteNumber;
 import roulette.RouletteNumber.Column;
 import roulette.RouletteNumber.Included;
 import roulette.RouletteNumber.Parity;
+import roulette.RouletteNumber.Row;
+import roulette.RouletteNumber.Sector;
 import utility.Pair;
 
 
 public class ManageRoulette {
-    public static final int COLOR_PAYOUT = 2;
-    public static final int PARITY_PAYOUT = 2;
-    public static final int NUMBER_PAYOUT = 36;
-    public static final int INCLUDED_PAYOUT = 2;
-    public static final int COLUMN_PAYOUT = 3;
-    public static final int ROWS_PAYOUT = 3;
+    public static final double COLOR_PAYOUT = 2.0;
+    public static final double PARITY_PAYOUT = 2.0;
+    public static final double NUMBER_PAYOUT = 36.0;
+    public static final double INCLUDED_PAYOUT = 2.0;
+    public static final double COLUMN_PAYOUT = 3.0;
+    public static final double ROW_PAYOUT = 3.0;
+    public static final double ZERO_PAYOUT = 36.0 / 7;
+    public static final double VOISINS_PAYOUT = 36.0 / 17;
+    public static final double TIER_PAYOUT = 36.0 / 12;
+    public static final double ORPHELINS_PAYOUT = 36.0 / 8;
     
-    public double calculateWin(final List<Pair<Object, Double>> bet, final RouletteNumber winningNumber) {
-        double win = 0;
-        for (final Pair<Object, Double> p : bet) {
+    public double calculateWin(final List<Pair<Object, Double>> bets, final RouletteNumber winningNumber) {
+        double win = 0.0;
+        for (final Pair<Object, Double> p : bets) {
             final Object property = p.getX();
             final double amount = p.getY();
             final Class<? extends Object> classProperty = property.getClass();
             
-            if (classProperty == Color.class) {
-                if ((Color) property == winningNumber.getColor()) {
-                    win += amount * COLOR_PAYOUT;
-                } else {
-                    win -= amount;
-                }
-            } else if (classProperty == Integer.class) {
-                if ((Integer) property == winningNumber.getValue()) {
+            if (property == winningNumber.getProperty(classProperty)
+                    || (property == Sector.VOISINS && winningNumber.getSector() == Sector.ZERO)) {
+                if (classProperty == Integer.class) {
                     win += amount * NUMBER_PAYOUT;
-                } else {
-                    win -= amount;
+                } else if (classProperty == Color.class || classProperty == Parity.class
+                        || classProperty == Included.class) {
+                    win += amount * COLOR_PAYOUT;
+                } else if (classProperty == Row.class || classProperty == Column.class) {
+                    win += amount * ROW_PAYOUT;
+                } else if (classProperty == Sector.class) {
+                    switch ((Sector) property) {
+                        case ZERO:
+                            win += amount * ZERO_PAYOUT;
+                            break;
+                        case VOISINS:
+                            win += amount * VOISINS_PAYOUT;
+                            break;
+                        case TIER:
+                            win += amount * TIER_PAYOUT;
+                            break;
+                        case ORPHELINS:
+                            win += amount * ORPHELINS_PAYOUT;
+                            break;
+                        default:
+                            break;
+                    }
                 }
-            } else if (classProperty == Parity.class) {
-                if ((Parity) property == winningNumber.getParity()) {
-                    win += amount * PARITY_PAYOUT;
-                } else {
-                    win -= amount;
-                }
-            } else if (classProperty == Included.class) {
-                if ((Included) property == winningNumber.getIncluded()) {
-                    win += amount * INCLUDED_PAYOUT;
-                } else {
-                    win -= amount;
-                }
-            } else if (classProperty == Column.class) {
-                if ((Column) property == winningNumber.getColumn()) {
-                    win += amount * COLUMN_PAYOUT;
-                } else {
-                    win -= amount;
-                }
-            }
+            } else {
+                win -= amount;
+            }    
         }
+        win = Math.round(win * 100.0) / 100.0;
+        //System.out.println(win);
         return win;
     }
     
-    private boolean checkWin(final RouletteNumber winningNumber, final Object property) {
-        if (property.getClass() == Color.class) {
-            return property == winningNumber.getColor();
-        }
-        return false;
-    }
+//    private boolean checkWin(final RouletteNumber winningNumber, final Object property) {
+//        if (property.getClass() == Color.class) {
+//            return property == winningNumber.getColor();
+//        }
+//        return false;
+//    }
     
 
 }
