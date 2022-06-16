992e8079afe69fb78286ab4046405802636bc2b0
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-06-16 20:33:31 +0200

    Upgraded RouletteGame with more function

--- src/view/menu/games/roulette/RouletteGame.java
+++ src/view/menu/games/roulette/RouletteGame.java
@@ -6,9 +6,11 @@ import java.util.List;
 
 import javax.swing.JPanel;
 import roulette.Roulette;
+import roulette.RouletteNumber;
 import roulette.RouletteNumbers;
 import roulette.manageRoulette.ManageRoulette;
 import utility.Pair;
+import view.menu.GeneralGui2;
 import view.menu.games.Game;
 
 
@@ -19,24 +21,28 @@ public class RouletteGame extends JPanel implements Game {
     private final DisplayWinningNumbers winningNumbers;
     private final Table table;
     private final ManageRoulette win;
+    private final GeneralGui2 generalInterface;
     
-    public RouletteGame(final Dimension dimension) {
+    public RouletteGame(final Dimension dimension, final GeneralGui2 generalInterface) {
         this.setLayout(new BorderLayout());
         
+        this.generalInterface = generalInterface;
         this.roulette = new Roulette();
-        this.winningNumbers = new DisplayWinningNumbers(roulette, new Dimension(dimension.width, dimension.height/8));
+        this.winningNumbers = new DisplayWinningNumbers(new Dimension(dimension.width, dimension.height / 10));
         this.add(this.winningNumbers, BorderLayout.NORTH);
-        this.table = new Table();
+        this.table = new Table(generalInterface);
         this.add(table);
         this.win = new ManageRoulette();
     }
 
     @Override
     public void endBetting() {
+        final RouletteNumber rouletteNumber= roulette.spin();
         final List<Pair<Object, Double>> bets = table.endBetting();
-        final double win = this.win.calculateWin(bets, new RouletteNumbers().get(0));
-        System.out.println("La vincita è di: " + win);
-        winningNumbers.display();
+        final double win = this.win.calculateWin(bets, rouletteNumber);
+        winningNumbers.update(rouletteNumber);
+        generalInterface.setWinMessage(win);
+        
     }
 
     @Override
