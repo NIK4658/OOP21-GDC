81c9cde15148adb839da4631feaa93b205c0d266
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-06-18 14:16:03 +0200

    Update Roulette.java

--- src/roulette/Roulette.java
+++ src/roulette/Roulette.java
@@ -1,32 +1,9 @@
 package roulette;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import roulette.number.RouletteNumber;
 
-import javax.swing.Timer;
-
-import view.menu.games.roulette.DisplayWinningNumbers;
-
-//da eliminare winningNumbers e metodo spin deve tornare un RouletteNumber
-public class Roulette {
-//    private static final int DELAY = 3000;//da togliere o mettere poi gif roulette che gira
-    private final Random random;
-//    private final WinningNumbers winningNumbers;
+public interface Roulette {
     
-    public Roulette() {
-        this.random = new Random();
-//        this.winningNumbers = new WinningNumbers();
-    }
-    
-//    public void start() {
-//        System.out.println("Start");
-//        new Timer(DELAY, e -> this.spin()).start();
-//    }
+    RouletteNumber spin();
 
-    public RouletteNumber spin() {
-//        this.winningNumbers.add(this.random.nextInt(37));
-//        return new ArrayList<>(winningNumbers);
-        return new RouletteNumbers().get(this.random.nextInt(37));
-    }
 }
