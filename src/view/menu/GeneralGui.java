099073d2e1690d28a8c2c88c5587e28ccac887c4
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2022-06-20 23:39:02 +0200

    Implemented new fiches

--- src/view/menu/GeneralGui.java
+++ src/view/menu/GeneralGui.java
@@ -40,6 +40,13 @@ public class GeneralGui extends JPanel implements Menu {
     private final Game g;
     private final JButton reset;
     private final JButton confirm;
+    private final JButton fiches1;
+    private final JButton fiches5;
+    private final JButton fiches25;
+    private final JButton fiches100;
+    private final JButton fiches500;
+    private final int width;
+    private final int height;
     private final JLabel betValue = new JLabel("0€");
     private final JLabel winValue = new JLabel("0€");
     private final JLabel balanceValue = new JLabel("0€");
@@ -60,8 +67,8 @@ public class GeneralGui extends JPanel implements Menu {
         this.account = account;
         setLayout(new BorderLayout());
         this.setPreferredSize(frame.getSizeMenu());
-        final int width = frame.getWidthMenu();
-        final int height = frame.getHeightMenu();
+        this.width = frame.getWidthMenu();
+        this.height = frame.getHeightMenu();
         
         final JPanel north = new JPanel(new GridBagLayout());
         final JPanel south = new JPanel(new BorderLayout());
@@ -140,11 +147,11 @@ public class GeneralGui extends JPanel implements Menu {
         
         this.reset = new JButton();
         this.confirm = new JButton();
-        final JButton fiches1 = new JButton();
-        final JButton fiches5 = new JButton();
-        final JButton fiches25 = new JButton();
-        final JButton fiches100 = new JButton();
-        final JButton fiches500 = new JButton();
+        this.fiches1 = new JButton();
+        this.fiches5 = new JButton();
+        this.fiches25 = new JButton();
+        this.fiches100 = new JButton();
+        this.fiches500 = new JButton();
             
         final List<JButton> list = new ArrayList<>();
         list.add(this.reset);
@@ -158,11 +165,11 @@ public class GeneralGui extends JPanel implements Menu {
         final List<String> fichesList = new ArrayList<>();
         fichesList.add("cancel"); //da cambiare in pulsante reset
         fichesList.add("confirm"); //da cambiare in pulsante conferma
-        fichesList.add("1");
-        fichesList.add("5");
-        fichesList.add("25");
-        fichesList.add("100");
-        fichesList.add("500");
+        fichesList.add("1newhigh");
+        fichesList.add("5new");
+        fichesList.add("25new");
+        fichesList.add("100new");
+        fichesList.add("500new");
         
         //Adding fiches
         i = 0;
@@ -179,15 +186,22 @@ public class GeneralGui extends JPanel implements Menu {
                         + fichesList.get(i) + ".png"))
                         .getScaledInstance(width / 20, width / 20, Image.SCALE_SMOOTH)));
             } else {
-                jb.setIcon(new ImageIcon((ImageLoader.getImage("res/img/fiches/numbers/"
+                jb.setIcon(new ImageIcon((ImageLoader.getImage("res/img/fiches/numbers/new/"
                         + fichesList.get(i) + ".png"))
-                        .getScaledInstance(width / 20, width / 20, Image.SCALE_SMOOTH)));
+                        .getScaledInstance(width / 15, width / 15, Image.SCALE_SMOOTH)));
             }
-
-            if (i == 1) {
-                southright.add(jb, new MyGridBagConstraints(i, 0, 1, 2, new Insets(0, 0, height / 100, height / 30)));
+            
+            
+            if (i <= 1) {
+                if (i == 0) {
+                    southright.add(jb, 
+                            new MyGridBagConstraints(i, 0, 1, 2, new Insets(0, 0, height / 200, height / 100)));
+                } else {
+                    southright.add(jb, 
+                            new MyGridBagConstraints(i, 0, 1, 2, new Insets(0, 0, height / 200, height / 30)));
+                }
             } else {
-                southright.add(jb, new MyGridBagConstraints(i, 0, 1, 2, new Insets(0, 0, height / 100, height / 100)));
+                southright.add(jb, new MyGridBagConstraints(i, 0, 1, 2, new Insets(0, 0, height / 100, 0)));
             }
             i++;
         }
@@ -197,11 +211,30 @@ public class GeneralGui extends JPanel implements Menu {
         this.confirm.addActionListener(e -> this.g.confirmBet());
         
         //Adding Listener to set fiches value
-        fiches1.addActionListener(e -> fichesvalue = 1);
-        fiches5.addActionListener(e -> fichesvalue = 5);
-        fiches25.addActionListener(e -> fichesvalue = 25);
-        fiches100.addActionListener(e -> fichesvalue = 100);
-        fiches500.addActionListener(e -> fichesvalue = 500);
+        fiches1.addActionListener(e -> {
+            setSelectedFiches(0);
+            fichesvalue = 1;
+        });
+        
+        fiches5.addActionListener(e -> {
+            setSelectedFiches(1);
+            fichesvalue = 5;
+        });
+        
+        fiches25.addActionListener(e -> {
+            setSelectedFiches(2);
+            fichesvalue = 25;
+        });
+        
+        fiches100.addActionListener(e -> {
+            setSelectedFiches(3);
+            fichesvalue = 100;
+        });
+        
+        fiches500.addActionListener(e -> {
+            setSelectedFiches(4);
+            fichesvalue = 500;
+        });
         
         backToMenu.addActionListener(e -> frame.setMainMenu(account));
 
@@ -215,12 +248,12 @@ public class GeneralGui extends JPanel implements Menu {
         
 
         final JLayeredPane southtotal = new JLayeredPane();
-        southtotal.setPreferredSize(new Dimension(width / 4, height / 10));
-        southleft.setBounds(0, 0, width / 4, height / 10);
+        southtotal.setPreferredSize(new Dimension((int) (width / 3.5) + width / 60, height / 10));
+        southleft.setBounds(width / 60, 0,(int) (width / 3.5), height / 10);
         
         final JLabel jl = new JLabel(new ImageIcon(ImageLoader.getImage("res/img/gui/ProvaSfondoLabel4.png")
-                .getScaledInstance(width / 4, height / 10, Image.SCALE_SMOOTH)));
-        jl.setBounds(0, 0, width / 4, height / 10);
+                .getScaledInstance((int) (width / 3.5), height / 10, Image.SCALE_SMOOTH)));
+        jl.setBounds(width / 60, 0, (int) (width / 3.5), height / 10);
         
         southtotal.add(southleft, 0);
         southtotal.add(jl, 1);
@@ -255,6 +288,34 @@ public class GeneralGui extends JPanel implements Menu {
         betValue.setText(value + "€");
     }
         
+    public void setSelectedFiches(final int fichesvalue) {
+        ArrayList<JButton> list = new ArrayList<>();
+        list.add(fiches1);
+        list.add(fiches5);
+        list.add(fiches25);
+        list.add(fiches100);
+        list.add(fiches500);
+        
+        
+        final List<String> fichesList = new ArrayList<>();
+        fichesList.add("1new");
+        fichesList.add("5new");
+        fichesList.add("25new");
+        fichesList.add("100new");
+        fichesList.add("500new");
+        
+        int i = 0;
+        for(JButton jb : list) {
+            if(fichesvalue==i) {
+                jb.setIcon(new ImageIcon((ImageLoader.getImage("res/img/fiches/numbers/new/"+fichesList.get(i)+"high.png")).getScaledInstance(width / 15, width / 15, Image.SCALE_SMOOTH)));   
+            } else {
+                jb.setIcon(new ImageIcon((ImageLoader.getImage("res/img/fiches/numbers/new/"+fichesList.get(i)+".png")).getScaledInstance(width / 15, width / 15, Image.SCALE_SMOOTH)));
+            }
+            i++;
+        }
+    }
+    
+    
     public void setWinValue(final double value) {
         winValue.setText(value + "€");
     }
