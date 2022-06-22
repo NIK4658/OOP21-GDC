ede01a7038479b6107599c3be27c6ea913f38770
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-06-22 18:07:19 +0200

    Refactoring Roulette

--- src/view/menu/games/roulette/Table.java
+++ src/view/menu/games/roulette/Table.java
@@ -32,17 +32,12 @@ public class Table extends JPanel {
     private int gridy;
     private GridBagConstraints gbc;
     private RouletteBetButton button;
-    private Dimension dim;
-    private final int width;
-    private final int height;
     private final List<RouletteBetButton> buttons;
     
     public Table(final GeneralGui generalInterface, final Games game) {
         this.generalInterface = generalInterface;
         this.game = game;
         this.setPreferredSize(generalInterface.getMenu().getPreferredSize());
-        width = this.getPreferredSize().width;
-        height = this.getPreferredSize().height;
         this.setLayout(new GridBagLayout());
         this.buttons = new LinkedList<>();
         
@@ -89,25 +84,20 @@ public class Table extends JPanel {
     }
     
     private void addSectors() {
-        dim = new Dimension(this.width / 14 * 3, this.height / 6);
         gbc = new MyGridBagConstraints(gridx, gridy, 3, 1);
         button = new RouletteBetButton(this.getPreferredSize(), Sector.TIER);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
         gbc.gridx += 3;
         button = new RouletteBetButton(this.getPreferredSize(), Sector.ORPHELINS);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
         gbc.gridx += 3;
         button = new RouletteBetButton(this.getPreferredSize(), Sector.VOISINS);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
         gbc.gridx += 3;
         button = new RouletteBetButton(this.getPreferredSize(), Sector.ZERO);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
     }
@@ -128,16 +118,12 @@ public class Table extends JPanel {
             button.setForeground(n.getColor());
             buttons.add(button);
             if (value == 0 && game == Games.ROULETTE_AMERICAN) {
-                button.setPreferredSize(new Dimension(width / 14, height / 6));
                 this.add(button, new MyGridBagConstraints(0, 1, 1, 1));
             } else if (value == AmericanRouletteNumbers._00_) {
-                button.setPreferredSize(new Dimension(width / 14, height / 6));
                 this.add(button, new MyGridBagConstraints(0, 3, 1, 1));
             } else if (value == 0) { 
-                button.setPreferredSize(new Dimension(width / 14, height / 2));
                 this.add(button, new MyGridBagConstraints(0, 1, 1, 3));
             } else {
-                button.setPreferredSize(new Dimension(width / 14, height / 6));
                 this.add(button, gbc);
                 gridy--;
                 if (gridy == 0) {
@@ -162,7 +148,6 @@ public class Table extends JPanel {
                 default: row = Row.THIRD;
             }
             button = new RouletteBetButton(this.getPreferredSize(), row);
-            button.setPreferredSize(new Dimension(width / 14, height / 6));
             buttons.add(button);
             this.add(button, gbc);
             gbc.gridy--;
@@ -173,19 +158,15 @@ public class Table extends JPanel {
         gridx -= 12;
         gridy += 4;
         gbc = new MyGridBagConstraints(gridx, gridy, 4, 1);
-        dim = new Dimension(width / 14 * 4, height / 6);
         button = new RouletteBetButton(this.getPreferredSize(), Column.FIRST);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
         gbc.gridx += 4;
         button = new RouletteBetButton(this.getPreferredSize(), Column.SECOND);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
         gbc.gridx += 4;
         button = new RouletteBetButton(this.getPreferredSize(), Column.THIRD);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
         
@@ -194,14 +175,11 @@ public class Table extends JPanel {
     private void addNumbersIncluded() {
         gridy += 1;
         gbc = new MyGridBagConstraints(gridx, gridy, 2, 1);
-        dim = new Dimension(width / 7, height / 6);
         button = new RouletteBetButton(this.getPreferredSize(), Included._1_18_);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
         gbc.gridx += 10;
         button = new RouletteBetButton(this.getPreferredSize(), Included._19_36_);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
     }
@@ -210,12 +188,10 @@ public class Table extends JPanel {
         gridx += 2;
         gbc.gridx = gridx;
         button = new RouletteBetButton(this.getPreferredSize(), Parity.EVEN);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
         gbc.gridx += 6;
         button = new RouletteBetButton(this.getPreferredSize(), Parity.ODD);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
     }
@@ -224,12 +200,10 @@ public class Table extends JPanel {
         gridx += 2;
         gbc.gridx = gridx;
         button = new RouletteBetButton(this.getPreferredSize(), Color.RED);
-        button.setPreferredSize(dim);
         buttons.add(button);
         this.add(button, gbc);
         gbc.gridx += 2;
         final RouletteBetButton ba = new RouletteBetButton(this.getPreferredSize(), Color.BLACK);
-        ba.setPreferredSize(dim);
         buttons.add(ba);
         this.add(ba, gbc);
     }
