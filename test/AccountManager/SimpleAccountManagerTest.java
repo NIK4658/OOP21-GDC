73e7b40b43dccb1402648d9e26d168602d486969
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2022-06-22 11:36:59 +0200

    Added AccountManagerTests

--- test/AccountManager/SimpleAccountManagerTest.java
+++ test/AccountManager/SimpleAccountManagerTest.java
@@ -1,15 +1,13 @@
-package AccountManager;
+package accountmanager;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
-
-import java.io.File;
 
 import account.AccountManager;
 import account.SimpleAccountManagerImpl;
-
+import java.io.File;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,59 +18,102 @@ import org.junit.Test;
 public class SimpleAccountManagerTest {
     
     private AccountManager simpleAccount;
+    private static final String DIRECTORYPATH = "res/json/users/";
     private File file;
+    private String username; 
+    private String usernamenew; 
+    private String password; 
+    private String passwordnew; 
+    private String age;
     
     @Before
     public void initAccount() {
         this.simpleAccount = new SimpleAccountManagerImpl();
-        this.file = new File("res/json/users/Test.Json");
+        this.username = "Test";
+        this.password = "Password";
+        this.usernamenew = "Test2";
+        this.passwordnew = "Password2";
+        this.age = "21";
+        this.file = new File(DIRECTORYPATH + this.username + ".Json");
         if (file.exists() && !file.isDirectory()) {
             file.delete();
         }
     }
-
-    //test Registrazione
-    @Test
-    public void testSimpleRegister() {
-        assertFalse(this.simpleAccount.checkExisting("Test"));
-        assertFalse(this.file.exists() && !file.isDirectory());
-        assertTrue(this.simpleAccount.register("Test", "Password", "20"));
-        assertTrue(this.simpleAccount.checkExisting("Test"));
-        assertTrue(this.file.exists() && !file.isDirectory());
-        assertTrue(this.simpleAccount.logger("Test", "Password"));
-        assertEquals(this.simpleAccount.getUsr(), "Test");
-        assertEquals(this.simpleAccount.getPsw(), "Password");
-        assertEquals(this.simpleAccount.getAge(), "20");    
-        
-        assertTrue(this.simpleAccount.changeUsr("Test2"));
-        assertEquals(this.simpleAccount.getUsr(), "Test2");  
+    
+    private void changeFile(final String name) {
+        this.file = new File(DIRECTORYPATH + name + ".Json");
+    }
+    
+    private void logIn() {
+        this.simpleAccount.register(this.username, this.password, this.age);
+        this.simpleAccount.logger(this.username, this.password);
     }
 
-    //test login
+    @Test
+    public void testSimpleRegister() {
+        this.file.delete();
+        assertFalse(this.file.exists() && !file.isDirectory()); //File inizialmente non esiste
+        assertTrue(this.simpleAccount.register(this.username, this.password, this.age)); //Eseguo registrazione corretta
+        assertTrue(this.file.exists() && !file.isDirectory());  //File ora esiste
+    }
+
     @Test
     public void testSimpleLogin() {
-        assertTrue(this.simpleAccount.logger("Username", "Password")); //User exist, expected True.
-        assertFalse(this.simpleAccount.logger("Usrnm", "Password"));   //User does not exist, expected false.
+        this.simpleAccount.register(this.username, this.password, this.age);
+        assertFalse(this.simpleAccount.logger("Usrnm", "12345"));   //User does not exist, expected false.
+        assertTrue(this.simpleAccount.logger(this.username, this.password)); //User exist, expected True.
     }
     
     @Test
     public void testCheckExisting() {
-        
+        logIn();
+        assertFalse(this.simpleAccount.checkExisting("Test123"));          //Non esiste
+        assertTrue(this.simpleAccount.checkExisting("Test"));              //Esiste, creato prima
+        assertTrue(this.simpleAccount.checkExisting(this.file.getName()
+                .replaceFirst("[.][^.]+$", ""))); //Esiste, creato prima
+        assertTrue(this.simpleAccount.checkExisting(this.username));       //Esiste, creato prima
+        assertTrue(this.file.exists() && !file.isDirectory());             //Esiste effettivamente il file creato prima
     }
     
     @Test
+    public void testGetFields() {
+        logIn();
+        assertEquals(this.simpleAccount.getUsr(), this.username);
+        assertEquals(this.simpleAccount.getPsw(), this.password);
+        assertEquals(this.simpleAccount.getAge(), this.age);  
+    }
+    
+    
+    @Test
     public void testChangeUsr() {
-        
+        logIn();
+        assertTrue(this.file.exists() && !file.isDirectory());
+        assertTrue(this.simpleAccount.changeUsr(this.usernamenew));
+        assertFalse(this.file.exists() && !file.isDirectory());
+        changeFile(this.usernamenew);
+        assertTrue(this.file.exists() && !file.isDirectory());
+        assertNotEquals(this.simpleAccount.getUsr(), this.username); 
+        assertEquals(this.simpleAccount.getUsr(), this.usernamenew);
+        changeFile(this.username);
+        assertTrue(this.simpleAccount.logger(this.usernamenew, this.password));
+        assertFalse(this.simpleAccount.logger(this.username, this.password));
+        assertTrue(this.simpleAccount.changeUsr(this.username));
     }
     
     @Test
     public void testChangePass() {
-        
+        logIn();
+        assertTrue(this.simpleAccount.changePass(this.passwordnew));
+        assertNotEquals(this.simpleAccount.getPsw(), this.password); 
+        assertEquals(this.simpleAccount.getPsw(), this.passwordnew); 
     }
     
+
     @Test
     public void testDeleteAcc() {
-        
+        logIn();
+        assertTrue(this.simpleAccount.deleteAcc(this.username));
+        assertFalse(this.simpleAccount.deleteAcc(this.usernamenew));
     }
     
     @After
