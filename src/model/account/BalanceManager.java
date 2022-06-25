8d14fd66831a7b01f00956bc926c87b1045eab97
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2022-06-25 13:42:15 +0200

    JavaDoc

--- src/model/account/BalanceManager.java
+++ src/model/account/BalanceManager.java
@@ -5,12 +5,34 @@ package model.account;
  */
 public interface BalanceManager {
 
+    /**
+     * Increase the balance by the requested amount.
+     * 
+     * @param amount Amount to be added to the user balance.
+     * @return true if any changes have been made, false otherwise. 
+     */
     boolean deposit(double amount);
 
+    /**
+     * Decrease the balance by the requested amount.
+     * 
+     * @param amount Amount to be subtracted to the user balance.
+     * @return true if any changes have been made, false otherwise. 
+     */
     boolean withdraw(double amount);
     
+    /**
+     * Change the balance of the account.
+     * 
+     * @param amount New balance of the account.
+     * @return true if any changes have been made, false otherwise. 
+     */
     boolean changeBalance(double balancenew);
     
+    /**
+     * Getter of Balance.
+     * @return the balance of the account. 
+     */
     double getBalance();
     
 }
